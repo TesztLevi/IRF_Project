3bde77b27265c6eaef33c0563ede6ab241e9c829
Levente Németh <NemethLevente@hotmail.hu>
2020-11-22 20:37:36 +0100

    Commit 0.1 / Entites

---
+++ IRF_XDQ857/IRF_XDQ857/Entities/Player.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IRF_XDQ857.Entities
+{
+    class Player
+    {
+        public string name { get; set; }
+        public int attack { get; set; }
+        public int defense { get; set; }
+
+        public int physical { get; set; }
+
+        public int prize { get; set; }
+
+
+    }
+}
---
+++ IRF_XDQ857/IRF_XDQ857/Entities/Team.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IRF_XDQ857.Entities
+{
+    class Team
+    {
+        public string name { get; set; }
+        public int attack { get; set; }
+        public int defense { get; set; }
+        public int physical { get; set; }
+
+        public Player P1;
+        public Player P2;
+        public Player P3;
+        public Player P4;
+        public Player P5;
+    }
+}
