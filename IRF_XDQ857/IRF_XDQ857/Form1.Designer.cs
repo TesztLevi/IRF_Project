53c094657fd46df19c0199270c157b7a79dbda07
Levente Németh <NemethLevente@hotmail.hu>
2020-12-01 21:29:21 +0100

    Felhasználó felület tervezés, csapat létrehozás

--- IRF_XDQ857/IRF_XDQ857/Form1.Designer.cs
+++ IRF_XDQ857/IRF_XDQ857/Form1.Designer.cs
@@ -28,36 +28,228 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox_T1P1 = new System.Windows.Forms.TextBox();
+            this.comboBox_T1P1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox_T1P2 = new System.Windows.Forms.ComboBox();
+            this.textBox_T1P2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.toolStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // dataGridView1
+            // label1
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 62;
-            this.dataGridView1.RowTemplate.Height = 28;
-            this.dataGridView1.Size = new System.Drawing.Size(480, 293);
-            this.dataGridView1.TabIndex = 0;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(46, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Hazai";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(58, 289);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Center";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(58, 383);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Csatár";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(58, 337);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Csatár";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(46, 119);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(60, 20);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Hátvéd";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(46, 238);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(60, 20);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Hátvéd";
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1,
+            this.toolStripButton2});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(1109, 34);
+            this.toolStrip1.TabIndex = 7;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(86, 29);
+            this.toolStripButton1.Text = "Új Meccs";
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(104, 29);
+            this.toolStripButton2.Text = "Visszavágó";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(129, 58);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 26);
+            this.textBox1.TabIndex = 8;
+            // 
+            // textBox_T1P1
+            // 
+            this.textBox_T1P1.Location = new System.Drawing.Point(129, 113);
+            this.textBox_T1P1.Name = "textBox_T1P1";
+            this.textBox_T1P1.Size = new System.Drawing.Size(207, 26);
+            this.textBox_T1P1.TabIndex = 9;
+            this.textBox_T1P1.TextChanged += new System.EventHandler(this.TextBox_T1P1_TextChanged);
+            // 
+            // comboBox_T1P1
+            // 
+            this.comboBox_T1P1.FormattingEnabled = true;
+            this.comboBox_T1P1.Location = new System.Drawing.Point(129, 154);
+            this.comboBox_T1P1.Name = "comboBox_T1P1";
+            this.comboBox_T1P1.Size = new System.Drawing.Size(207, 28);
+            this.comboBox_T1P1.TabIndex = 11;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(352, 151);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 31);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(352, 276);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 31);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // comboBox_T1P2
+            // 
+            this.comboBox_T1P2.FormattingEnabled = true;
+            this.comboBox_T1P2.Location = new System.Drawing.Point(129, 279);
+            this.comboBox_T1P2.Name = "comboBox_T1P2";
+            this.comboBox_T1P2.Size = new System.Drawing.Size(207, 28);
+            this.comboBox_T1P2.TabIndex = 14;
+            // 
+            // textBox_T1P2
+            // 
+            this.textBox_T1P2.Location = new System.Drawing.Point(129, 238);
+            this.textBox_T1P2.Name = "textBox_T1P2";
+            this.textBox_T1P2.Size = new System.Drawing.Size(207, 26);
+            this.textBox_T1P2.TabIndex = 13;
+            this.textBox_T1P2.Visible = false;
+            this.textBox_T1P2.TextChanged += new System.EventHandler(this.TextBox_T1P2_TextChanged);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(84, 455);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(145, 58);
+            this.button3.TabIndex = 16;
+            this.button3.Text = "Kész a Csapat";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.dataGridView1);
+            this.ClientSize = new System.Drawing.Size(1109, 632);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.comboBox_T1P2);
+            this.Controls.Add(this.textBox_T1P2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox_T1P1);
+            this.Controls.Add(this.textBox_T1P1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox_T1P1;
+        private System.Windows.Forms.ComboBox comboBox_T1P1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox_T1P2;
+        private System.Windows.Forms.TextBox textBox_T1P2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- IRF_XDQ857/IRF_XDQ857/Form1.cs
+++ IRF_XDQ857/IRF_XDQ857/Form1.cs
@@ -15,12 +15,19 @@ namespace IRF_XDQ857
     public partial class Form1 : Form
     {
         BindingList<Player> players = new BindingList<Player>();
+        BindingList<Team> teams = new BindingList<Team>();
 
         public Form1()
         {
             InitializeComponent();
             xmlLoad();
-            dataGridView1.DataSource = players; // ez csak adatellenőrésre van itt
+
+            
+            comboBox_T1P1.DataSource = players.ToList();
+            comboBox_T1P1.DisplayMember = "name";
+            comboBox_T1P2.DataSource = players.ToList();
+            comboBox_T1P2.DisplayMember = "name";
+
         }
 
         private void xmlLoad()
@@ -47,6 +54,88 @@ namespace IRF_XDQ857
 
                 player.prize = prize;
             }
+        }
+
+        private void TextBox_T1P1_TextChanged(object sender, EventArgs e)
+        {
+            Kereső_T1P1();
+        }
+
+        private void Kereső_T1P1()
+        {
+                           
+
+            comboBox_T1P1.DataSource = (from p in players
+                                    where p.name.StartsWith(textBox_T1P1.Text)
+                                    select p).ToList();
+        }
+
+        
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            comboBox_T1P1.Enabled = false;
+            textBox_T1P1.Visible = false;
+            textBox_T1P2.Visible = true;
+        }
+
+        private void TextBox_T1P2_TextChanged(object sender, EventArgs e)
+        {
+            Kereső_T1P2();
+        }
+
+        private void Kereső_T1P2()
+        {
+            comboBox_T1P2.DataSource = (from p in players
+                                        where p.name.StartsWith(textBox_T1P2.Text)
+                                        select p).ToList();
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            comboBox_T1P2.Enabled = false;
+            textBox_T1P2.Visible = false;
+            //textBox_T1P2.Visible = true;
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+           //player 1
+            Player t1p1 = (Player)comboBox_T1P1.SelectedItem;
+
+            var name1 = from n in players
+                     where n.name == t1p1.name
+                     select n.name;
+
+            var att1 = from p1a in players
+                      where p1a.attack == t1p1.attack
+                      select p1a.attack;
+
+            //player2
+
+
+            Player t1p2 = (Player)comboBox_T1P2.SelectedItem;
+
+            var name2 = from n in players
+                        where n.name == t1p2.name
+                        select n.name;
+
+            var att2 = from p1a in players
+                       where p1a.attack == t1p2.attack
+                       select p1a.attack;
+
+            Team t = new Team();
+
+            t.P1 = t1p1;
+            t.P2 = t1p2;
+            t.name = textBox1.Text;
+            t.attack = t1p1.attack + t1p2.attack;
+
+            string próba = (t.attack * 2).ToString();
+
+            teams.Add(t);
+            
+            MessageBox.Show("Csapatod neve:" + t.name + " Attack: " + próba);
         }
     }
 }
