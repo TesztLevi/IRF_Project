528a3ed264fbbed4fd18cfabf60cb21741e0c637
Levente Németh <NemethLevente@hotmail.hu>
2020-12-09 21:37:13 +0100

    Commit Market UC alap fejlesztései

--- IRF_XDQ857/IRF_XDQ857/Entities/Market.Designer.cs
+++ IRF_XDQ857/IRF_XDQ857/Entities/Market.Designer.cs
@@ -28,19 +28,91 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 198);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(747, 302);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(39, 42);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(145, 26);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(204, 42);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 26);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(324, 42);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 26);
+            this.textBox3.TabIndex = 3;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(441, 42);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 26);
+            this.textBox4.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(420, 100);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 39);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
             // 
             // Market
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
             this.Name = "Market";
-            this.Size = new System.Drawing.Size(395, 344);
+            this.Size = new System.Drawing.Size(750, 500);
+            this.Load += new System.EventHandler(this.Market_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button1;
     }
 }
--- IRF_XDQ857/IRF_XDQ857/Entities/Market.cs
+++ IRF_XDQ857/IRF_XDQ857/Entities/Market.cs
@@ -7,14 +7,77 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace IRF_XDQ857.Entities
 {
     public partial class Market : UserControl
     {
+        BindingList<Player> players = new BindingList<Player>();
         public Market()
         {
             InitializeComponent();
+            xmlLoad();
+        }
+
+        private void xmlLoad()
+        {
+            var xml = new XmlDocument();
+            xml.Load("XML/IRF_Players.xml");
+
+            foreach (XmlElement element in xml.DocumentElement)
+            {
+
+                var player = new Player();
+                players.Add(player);
+
+
+                player.name = (element.GetAttribute("name"));
+
+
+                var childElement = (XmlElement)element.ChildNodes[0];
+                player.attack = int.Parse(childElement.GetAttribute("att"));
+                player.defense = int.Parse(childElement.GetAttribute("def"));
+                player.physical = int.Parse(childElement.GetAttribute("phy"));
+
+                var prize = int.Parse(childElement.InnerText);
+
+                player.prize = prize;
+            }
+        }
+
+        private void Market_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+            //Cip cipő = (Cipők)listBox1.SelectedItem;
+
+            
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            Random r = new Random();
+
+            var attack = from a in players
+                         where a.attack > int.Parse(textBox1.Text) && a.defense > int.Parse(textBox2.Text)
+
+
+
+
+                         select new
+                         {
+                             Név = a.name,
+                             Támadás = a.attack + r.Next(-3,4),
+                             Védekezés = a.defense + r.Next(-3, 4),
+                             Fizikum = a.physical + r.Next(-3, 4),
+                             Ár = a.prize,
+
+                         };
+            dataGridView1.DataSource = attack.ToList();
         }
     }
 }
